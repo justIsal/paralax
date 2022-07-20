60178b7fc1880adfb95dee52d3b8ad6f8ad082e5
tsalman <tsalmanisal@gmail.com>
2022-07-20 12:54:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector('nav ul');
+const toggle = document.querySelector('.menu-toggle input')
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('slide')
+});
+
+let getHead = document.getElementsByClassName("header")[0]
+let getParag = document.getElementsByClassName('paragraf')[0];
+let getBg = document.querySelector('#home .img-bg')
+let getBurung = document.querySelector('#home .img-burung')
+let getPerahu = document.querySelector('#home .img-perahu')
+let getText = document.querySelector('#home h1')
+let getTop = document.querySelector('.top p')
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY
+    getBg.style.top = value + 0.2 + 'px'
+    getBurung.style.right = 100 + value + 0.9 + 'px'
+    getBurung.style.top =value + 0.1 + 'px'
+    getText.style.top = 200 + value + -5 + 'px'
+})
+getTop.addEventListener('click', () => {
+    return location.href='#home'
+})
